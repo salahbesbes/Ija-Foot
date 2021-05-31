c62a967fbab68718109dc1c6db00c3e67c17c626
salah_besbes <salah_besbes@ymail.com>
2021-05-31 17:11:30 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text> this is home page </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+to generate SH1 : cd android && ./gradlew signingReport
+make sure to grab the SH1 from :app:signingReport
+# Ija-Foot

df3d0e801a5214885505add3217c11683c990bfb
benhassenwael <greenw2012@gmail.com>
2021-06-03 16:23:11 +0100

    google signIn will not update existing user anymore

--- hooks/useSignIn.js
+++ hooks/useSignIn.js
@@ -1,5 +1,4 @@
 import auth from '@react-native-firebase/auth';
-import db from '@react-native-firebase/firestore';
 
 import {useCallback, useContext} from 'react';
 import {actionCreators} from '../stateManager/actions/auth-A';
